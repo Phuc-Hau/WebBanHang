dba0fe8eb12eb7f3b4c0f51b66cc54b84d122d57
Dannguyen2206 <91892771+Dannguyen2206@users.noreply.github.com>
2022-11-13 12:08:45 +0700

    dsx

---
+++ src/main/java/com/webbanhang/jpa/model/OrderStatus.java
@@ -0,0 +1,23 @@
+package com.webbanhang.jpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "orderstatus")
+@Entity
+public class OrderStatus {
+    @Id
+    @Column(name = "Id", nullable = false)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
