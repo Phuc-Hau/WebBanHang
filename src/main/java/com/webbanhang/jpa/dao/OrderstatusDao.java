8903cafb7c0d9c1c4a245453ab1ef91f749e25c2
Dannguyen2206 <91892771+Dannguyen2206@users.noreply.github.com>
2022-11-13 11:53:36 +0700

    Dan
    Orderstatus

--- src/main/java/com/webbanhang/jpa/dao/OrderstatusDao.java
+++
@@ -1,4 +0,0 @@
-package com.webbanhang.jpa.dao;
-
-public interface OrderstatusDao {
-}
--- src/main/resources/static/js/angular/Orderstatus.js
+++ src/main/resources/static/js/angular/Orderstatus.js
@@ -1,1 +1,15 @@
 var app = angular.module("app", []);
+
+app.controller('orderstatus', function($scope,$http) {
+
+    $scope.url="/admin/api/home";
+    $scope.items =[];
+
+    $http.get($scope.url).then(resp => {
+        $scope.items = resp.data;
+        console.log("Success", resp)
+    }).catch(error => {
+        console.log("fail", error)
+    })
+
+});
